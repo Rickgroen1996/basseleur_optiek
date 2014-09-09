eaaeaffd31c187e0a80b9266131eb9aa973a784d
jairzijp <jairzijp123@gmail.com>
2014-09-09 16:58:41 +0200

    menu

--- www/index.php
+++ www/index.php
@@ -14,7 +14,48 @@
 <body>
 
     <div class="row">
-			
+		<div class="off-canvas-wrap" data-offcanvas>
+  <div class="inner-wrap">
+    <nav class="tab-bar">
+      <section class="left-small">
+        <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
+      </section>
+
+      <section class="middle tab-bar-section">
+        <h1 class="title">Foundation</h1>
+      </section>
+
+      <section class="right-small">
+        <a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a>
+      </section>
+    </nav>
+
+    <aside class="left-off-canvas-menu">
+      <ul class="off-canvas-list">
+        <li><label>Foundation</label></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        <li><a href="#">...</a></li>
+      </ul>
+    </aside>
+
+    <aside class="right-off-canvas-menu">
+      <ul class="off-canvas-list">
+        <li><label>Users</label></li>
+        <li><a href="#">Hari Seldon</a></li>
+        <li><a href="#">...</a></li>
+      </ul>
+    </aside>
+
+    <section class="main-section">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus porta arcu sit amet ultrices. Proin pretium faucibus sem, volutpat pretium eros laoreet quis. Nulla facilisi. Praesent lobortis purus nec leo malesuada, ac ornare erat vulputate. Cras sagittis leo sed venenatis aliquet. Curabitur pellentesque ipsum massa, id aliquam ex gravida vel. Etiam a eleifend lectus, quis lobortis quam. Vestibulum scelerisque odio a massa faucibus scelerisque.
+
+Vestibulum at ullamcorper nulla, eu lacinia mauris. Integer vel risus massa. Maecenas malesuada, lacus quis imperdiet blandit, ante urna pulvinar nisi, eget vehicula felis leo quis leo. Praesent dapibus ex ante, sit amet faucibus metus posuere in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam scelerisque, ipsum ac dictum vulputate, nunc sem aliquet orci, quis vehicula dolor metus et justo. Praesent rhoncus mollis quam sed ornare. Nunc vel ligula porta, lacinia sem luctus, porttitor ante. Donec suscipit eros in sem sagittis, ac laoreet purus condimentum. Morbi malesuada nisi venenatis libero ultrices tincidunt. Praesent nec felis sed dui eleifend consectetur. Aliquam pretium blandit elit, non pretium quam. Mauris quam augue, condimentum ut vulputate in, consequat id elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed velit risus, suscipit quis diam ut, molestie sodales ex.
+    </section>
+
+  <a class="exit-off-canvas"></a>
+
+  </div>
+</div>	
     </div>
 
     <script src="js/vendor/jquery.js"></script>
