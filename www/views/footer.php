a9dbc2c1c23eea0d693c9863118cb7e1994c4fe4
rickgroen1996 <rick.groen@hotmail.com>
2014-09-09 18:20:54 +0200

    footer, includes

---
+++ www/views/footer.php
@@ -0,0 +1,35 @@
+<div class="row">
+	<div class="columns panel small-12 medium-6 large-4">
+		<ul>
+			<li>Oogtest- en meting</li>
+			<li>Optometrie</li>
+			<li>Glasadvies</li>
+			<li>Contactlenzen</li>
+			<li>Monturen</li>
+			<li>Diversen</li>
+		</ul>
+	</div>
+	<div class="columns panel small-12 medium-6 large-4">
+		<ul>
+			<li>Over Basseleur</li>
+			<li>Zorgvergoedingen</li>
+			<li>Acties en nieuws</li>
+			<li>Medewerkers</li>
+			<li>Service en garantie</li>
+		</ul>
+	</div>
+	<div class="columns panel small-12 medium-6 large-4">
+		<ul>
+			<li>Home</li>
+			<li>Contact</li>
+			<li>Route</li>
+			<li>Online afspraak maken</li>
+		</ul>
+	</div>
+	<div class="columns panel small-12 medium-6 large-4">
+		<img src="img/logofooter.png" alt="footer logo">
+		<p>De Stient 12, 1132 BE Volendam</p>
+		<br>
+		<p>Postbus 154, 1130 AD, Tel: 0229-320060</p>
+	</div>
+</div>
