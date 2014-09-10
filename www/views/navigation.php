32a97f69cfcee0b37d5e7153e2918c1d1a2cda39
jairzijp <jairzijp123@gmail.com>
2014-09-10 22:27:02 +0200

    nav met submenu

--- www/views/navigation.php
+++ www/views/navigation.php
@@ -6,16 +6,19 @@
         <div class="large-9 medium-12 text-center columns">
         <div id='cssmenu'>
             <ul>
-                <li>
+                <li class="active">
                     <a href='#'>Home</a>
                 </li>
                 <li>
                     <a href='#'>Over</a>
                 
                 </li>
-                <li>
-                    <a href='#'>Zorgvergoedingen</a>
-                </li>
+                <li class='has-sub'><a href='#'>Producten & Diensten</a>
+                      <ul>
+                         <li class='has-sub'><a href='#' class="sub-item">Voorbeeld 1</a></li>
+                 <li class='has-sub'><a href='#' class="sub-item">Voorbeeld 2</a></li>
+                  </ul>
+               </li>
                 <li>
                     <a href='#'>Acties & Nieuws</a>
                 </li>
