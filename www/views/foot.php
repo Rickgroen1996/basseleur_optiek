ce463b068ae37f1c8c4aa23f3d3915096a2ab358
jairzijp <jairzijp123@gmail.com>
2014-09-14 17:15:52 +0200

    beetje styling

--- www/views/foot.php
+++ www/views/foot.php
@@ -1,7 +1,7 @@
 
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
-<script src="js/nav.js"></script>
+    <script src="js/nav.js"></script>
     <script src="js/script.js"></script>
     <script>
     $(document).foundation();
--- www/views/footer.php
+++ www/views/footer.php
@@ -1,37 +1,39 @@
 <div class="row">
-    <div class="large-12">
-	<div class="columns small-12 medium-6 large-3">
+    
+    <div class="large-12 footer columns ">
+	<div class="info_footer columns small-12 text-center medium-4 large-3 columns">
 		<ul>
-			<li>Oogtest- en meting</li>
-			<li>Optometrie</li>
-			<li>Glasadvies</li>
-			<li>Contactlenzen</li>
-			<li>Monturen</li>
-			<li>Diversen</li>
+			Openingstijden
+
+            <li>maandag  -  13.00-18.00 uur</li>            
+			<li> di t/m do  -  9.00-18.00 uur</li>
+			<li>vrijdag  -  9.00-19.00 uur</li>
+			<li>zaterdag  -  9.00-16:30 uur</li>
 		</ul>
 	</div>
-	<div class="columns small-12 medium-6 large-3">
+	<div class="info_footer columns small-6 medium-4 large-3 columns">
 		<ul>
-			<li>Over Basseleur</li>
-			<li>Zorgvergoedingen</li>
-			<li>Acties en nieuws</li>
-			<li>Medewerkers</li>
-			<li>Service en garantie</li>
+			Algemeen
+			<li><a href="#">Over Basseleur</a></li>
+			<li><a href="#">Acties en nieuws</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Online afspraak</a></li>
 		</ul>
 	</div>
-	<div class="columns small-12 medium-6 large-3">
+	<div class="info_footer columns small-6 medium-4 large-3 columns">
 		<ul>
-			<li>Home</li>
-			<li>Contact</li>
-			<li>Route</li>
-			<li>Online afspraak maken</li>
+            Producten en diensten
+			<li><a href="#">Lenzen</a></li>
+			<li><a href="#">Brillen</a></li>
+			<li><a href="#">Voorbeeld</a></li>
+			<li><a href="#">Voorbeeld</a></li>
 		</ul>
 	</div>
-	<div class="columns small-12 medium-6 large-3">
+	<div class=" info_footer columns text-center small-12 medium-12 large-3 columns ">
 		<img src="img/logofooter.png" alt="footer logo">
-		<p>De Stient 12, 1132 BE Volendam</p>
-		<br>
-		<p>Postbus 154, 1130 AD, Tel: 0229-320060</p>
+		<p>De Stient 12<br> 1132 BE Volendam <br> Postbus 154 1130 AD <br> Tel: 0229-320060 <br> <a href="#">info@basseleur.nl</a></p>
+		
+		
 	</div>
-        </footer>
+        
 </div>
